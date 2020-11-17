8b67d28e67a0f8efbe1c020d3099a527e9fff3e3
smashflashtech <smash.flash.tech@gmail.com>
2020-11-17 10:38:04 -0800

    establishes crud for post

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,56 +2,56 @@ const db = require('../models')
 
 //working
 const index = (req, res) => {
-    db.game.findAll().then((foundGames) => {
-        if (!foundGames) return res.json({
-            message: 'No Games found in database.'
+    db.post.findAll().then((foundPosts) => {
+        if (!foundPosts) return res.json({
+            message: 'No Posts found in database.'
         })
 
-        res.status(200).json({ games: foundGames });
+        res.status(200).json({ posts: foundPosts });
     })
 }
 
-//not working
+//showing single post
 const show = (req, res) => {
     console.log('in the show route')
     console.log(req.params)
     //not sure React side?
-    db.game.findByPk(req.params.id).then((foundGame) => {
-        if (!foundGame) return res.json({
-            message: 'Game with provided ID not found.'
+    db.post.findByPk(req.params.id).then((foundPost) => {
+        if (!foundPost) return res.json({
+            message: 'Post with provided ID not found.'
         })
 
-        res.status(200).json({ game: foundGame })
+        res.status(200).json({ post: foundPost })
     })
 }
 
 //working
 const create = (req, res) => {
-    db.game.create(req.body).then((savedGame) => {
+    db.post.create(req.body).then((savedPost) => {
         // Validations and error handling here
-        res.status(200).json({ game: savedGame })
+        res.status(200).json({ post: savedPost })
     })
 }
 
 //not sure need to get show page working first
 const update = (req, res) => {
-    db.game.update({
+    db.post.update({
         ...req.body
     }, {
         where: {
             id: req.params.id
         }
-    }).then((updatedGame) => {
-        if (!updatedGame) return res.json({
-            message: "No game with that ID found."
+    }).then((updatedPost) => {
+        if (!updatedPost) return res.json({
+            message: "No post with that ID found."
         })
         // Validations and error handling here
-        res.status(200).json({ game: updatedGame })
+        res.status(200).json({ post: updatedPost })
     })
 }
 //not sure
 const destroy = (req, res) => {
-    db.game.destroy({
+    db.post.destroy({
         where: { id: req.params.id }
     }).then(() => {
         res.status(200)
