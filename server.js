5513ffdc92d45791f36339183bd11683f11741b2
smashflashtech <smash.flash.tech@gmail.com>
2020-11-17 10:30:26 -0800

    fixes middleware to correct routes

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ app.use((req, res, next) => {
 
 
 // middleware - API routes
-app.use('/api/v1/games', routes.comments)
-app.use('/api/v1/games', routes.images)
-app.use('/api/v1/games', routes.pets)
-app.use('/api/v1/games', routes.posts)
+app.use('/api/v1/comments', routes.comments)
+app.use('/api/v1/images', routes.images)
+app.use('/api/v1/pets', routes.pets)
+app.use('/api/v1/posts', routes.posts)
 app.use('/api/v1/auth', routes.auth)
 
 // connection
